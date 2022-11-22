484cdfc5b55daed5a4a14879d7ef0ef43264271a
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2022-11-22 16:15:12 +0100

    list items added and styled, no js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const button = document.getElementById("add");
+
+//user clicked on the add button
+// if there's any text inside item field, added to to do list
+button.addEventListener("click", function () {
+  let value = document.getElementById("item").value;
+
+  // if (value) means if a value existed, so no empty string
+  if (value) {
+    console.log(value);
+  }
+});
